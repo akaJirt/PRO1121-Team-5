172d969eb28cdbf4d6e8a767c83962fb8b2171de
MinhTri <luongminhtri2411@gmail.com>
2023-07-14 17:52:06 +0700

    Sign up layout

--- app/src/main/java/com/example/hotel_luxvoy/OnboardingSlider.java
+++ app/src/main/java/com/example/hotel_luxvoy/OnboardingSlider.java
@@ -3,20 +3,36 @@ package com.example.hotel_luxvoy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.ItemChangeListener;
+import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 
 import java.util.ArrayList;
 
 public class OnboardingSlider extends AppCompatActivity {
+    TextView tvTitle, tvDescription;
+
+    ImageView imgSignUp, imgSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_slider);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvDescription = findViewById(R.id.tvDescription);
+
+        imgSignUp = findViewById(R.id.imgSignUp);
+        imgSignIn = findViewById(R.id.imgSignIn);
+
         ArrayList<SlideModel> imageList = new ArrayList<>();
         imageList.add(new SlideModel(R.drawable.slider1, ScaleTypes.FIT));
         imageList.add(new SlideModel(R.drawable.slider2, ScaleTypes.FIT));
@@ -25,6 +41,33 @@ public class OnboardingSlider extends AppCompatActivity {
         ImageSlider imageSlider = findViewById(R.id.image_slider);
         imageSlider.setImageList(imageList);
 
+        imageSlider.setItemChangeListener(new ItemChangeListener() {
+            @Override
+            public void onItemChanged(int position) {
+                switch (position) {
+                    case 0:
+                        updateSlideContent("Discover the beauty of Vietnam", "230+ Hotels to earn and redeem your point.");
+                        break;
+                    case 1:
+                        updateSlideContent("Travel with ease", "Book, check in and make requests from anywhere.");
+                        break;
+                    case 2:
+                        updateSlideContent("Elevate your experience", "Gain access to special rates and exclusive experiences.");
+                        break;
+                }
+            }
+        });
 
+        imgSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OnboardingSlider.this, SignUp.class));
+            }
+        });
+    }
+
+    private void updateSlideContent(String title, String description) {
+        tvTitle.setText(title);
+        tvDescription.setText(description);
     }
 }
