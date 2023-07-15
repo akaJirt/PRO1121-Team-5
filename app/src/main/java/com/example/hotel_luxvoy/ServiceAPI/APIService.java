52d3add8b53131dab0b78469d6285704cf4a7ff2
X <nhatnlps27103@fpt.edu.vn>
2023-07-15 19:58:40 +0700

    add some code

---
+++ app/src/main/java/com/example/hotel_luxvoy/ServiceAPI/APIService.java
@@ -0,0 +1,23 @@
+package com.example.hotel_luxvoy.ServiceAPI;
+
+import com.example.hotel_luxvoy.models.UserModel;
+import com.example.hotel_luxvoy.models.UserPostModel;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    @GET("users")
+    Call<ArrayList<UserModel>> getData();
+
+    @POST("users")
+    Call<UserPostModel> createUser(@Body UserPostModel user);
+
+}
--- app/src/main/java/com/example/hotel_luxvoy/activity/SignUpActivity.java
+++ app/src/main/java/com/example/hotel_luxvoy/activity/SignUpActivity.java
@@ -7,11 +7,24 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hotel_luxvoy.R;
+import com.example.hotel_luxvoy.ServiceAPI.APIService;
+import com.example.hotel_luxvoy.models.UserModel;
+import com.example.hotel_luxvoy.models.UserPostModel;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -46,6 +59,30 @@ public class SignUpActivity extends AppCompatActivity {
 
         btnSignUp = findViewById(R.id.btnSignUp);
 
+        //retrofit API
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("172.16.92.242:6969/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = retrofit.create(APIService.class);
+
+        //btn sign up
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                String fullName = edtFullName.getText().toString();
+                String phoneNumber = edtPhoneNumber.getText().toString();
+                String ward = edtWard.getText().toString();
+                String district = edtDistrict.getText().toString();
+                String street = edtStreet.getText().toString();
+
+                postData(userName, password, fullName, phoneNumber, ward, district, street);
+            }
+        });
+
         tvSignIn.setOnClickListener(v -> {
             Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
             startActivity(intent);
@@ -68,6 +105,30 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+    }
+
+    private void postData(String userName, String password, String fullName, String phoneNumber, String ward, String district, String street) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("172.16.92.242:6969/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = retrofit.create(APIService.class);
+        UserPostModel userPostModel = new UserPostModel(userName, password, fullName, Integer.parseInt(phoneNumber), ward, district, street);
+        Call<UserPostModel> call = apiService.createUser(userPostModel);
+        call.enqueue(new Callback<UserPostModel>() {
+            @Override
+            public void onResponse(Call<UserPostModel> call, Response<UserPostModel> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(SignUpActivity.this, "Đăng ký Thành Công!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserPostModel> call, Throwable t) {
+                Toast.makeText(SignUpActivity.this, "Đăng ký Thất Bại!", Toast.LENGTH_SHORT).show();
             }
         });
     }
