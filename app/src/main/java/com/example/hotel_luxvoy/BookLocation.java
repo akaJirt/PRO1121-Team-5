a3b41edc5458ce38a7b4fc8a1916f1a2a204a5c3
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-07-18 12:42:06 +0700

    BookLocationScreen
    add more screen book, find by location (HoChiMinh, HaNoi, HaLong)

--- app/src/main/java/com/example/hotel_luxvoy/BookLocation.java
+++ app/src/main/java/com/example/hotel_luxvoy/BookLocation.java
@@ -2,16 +2,13 @@ package com.example.hotel_luxvoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class BookActivity extends AppCompatActivity {
+public class BookLocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book);
-
-        startActivity(new Intent(BookActivity.this, BookLocation.class));
+        setContentView(R.layout.activity_book_location);
     }
 }
