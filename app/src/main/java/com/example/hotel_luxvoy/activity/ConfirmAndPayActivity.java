01d1f544505e4cbaa72deced522591b3e0f7a4bd
MinhTri <luongminhtri2411@gmail.com>
2023-07-18 16:37:17 +0700

    update code 18/7

--- app/src/main/java/com/example/hotel_luxvoy/activity/ConfirmAndPayActivity.java
+++ app/src/main/java/com/example/hotel_luxvoy/activity/ConfirmAndPayActivity.java
@@ -14,5 +14,6 @@ public class ConfirmAndPayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FullScreenHelper.setFullScreen(this);
         setContentView(R.layout.activity_confirm_and_pay);
+        //test
     }
 }
