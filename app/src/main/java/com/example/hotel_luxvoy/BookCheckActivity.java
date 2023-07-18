e761076ec7dc39d1191ed65cf59fe8ead3235a0e
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-07-18 13:47:37 +0700

    BookCheckScreen
    find hotel by: location, date (checkin, checkout), room, guest

--- app/src/main/java/com/example/hotel_luxvoy/BookCheckActivity.java
+++ app/src/main/java/com/example/hotel_luxvoy/BookCheckActivity.java
@@ -2,16 +2,13 @@ package com.example.hotel_luxvoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class BookLocation extends AppCompatActivity {
+public class BookCheckActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_location);
-
-        startActivity(new Intent(BookLocation.this, BookCheckActivity.class));
+        setContentView(R.layout.activity_book_check);
     }
 }
