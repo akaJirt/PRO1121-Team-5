7bfa879bc988161d52df82ebd71ce6df9e33184d
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-07-24 23:32:50 +0700

    edit code
    fragment, activity

--- app/src/main/java/com/example/hotel_luxvoy/BookActivity.java
+++ app/src/main/java/com/example/hotel_luxvoy/BookActivity.java
@@ -1,16 +1,29 @@
 package com.example.hotel_luxvoy;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BookActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        TextView txtWhere = findViewById(R.id.txtWhere);
+        TextView txtSelect = findViewById(R.id.txtDate);
 
 //        startActivity(new Intent(BookActivity.this, BookLocation.class));
     }
