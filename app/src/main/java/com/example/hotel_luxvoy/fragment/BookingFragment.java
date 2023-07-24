033c3e3814b61ca3f9852bfa4f7c88898d68c7f0
MinhTri <luongminhtri2411@gmail.com>
2023-07-25 04:09:09 +0700

    update code 25/7

--- app/src/main/java/com/example/hotel_luxvoy/fragment/BookingFragment.java
+++ app/src/main/java/com/example/hotel_luxvoy/fragment/BookingFragment.java
@@ -3,20 +3,100 @@ package com.example.hotel_luxvoy.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hotel_luxvoy.R;
+import com.example.hotel_luxvoy.adapter.ExploreAdaper;
+import com.example.hotel_luxvoy.adapter.LuxuryAdapter;
+import com.example.hotel_luxvoy.adapter.NearbyAdapter;
+import com.example.hotel_luxvoy.adapter.PremiumAdapter;
+import com.example.hotel_luxvoy.adapter.ViewedAdapter;
+import com.example.hotel_luxvoy.models.Explore;
+import com.example.hotel_luxvoy.models.Luxury;
+import com.example.hotel_luxvoy.models.Nearby;
+import com.example.hotel_luxvoy.models.Premium;
+import com.example.hotel_luxvoy.models.Viewed;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BookingFragment extends Fragment {
 
+    RecyclerView rvNearby, rvViewed, rvDestination, rvLuxury, rvPremium;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        rvNearby = rootView.findViewById(R.id.rvNearby);
+        rvNearby.setHasFixedSize(true);
+        rvNearby.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<Nearby> nearbyList = new ArrayList<>();
+        nearbyList.add(new Nearby(R.drawable.saigon1, "Luxvoy Luxury Hotel South Sai Gon", "3.8 Km"));
+        nearbyList.add(new Nearby(R.drawable.saigon2, "Luxvoy Luxury Hotel South Sai Gon 2", "4.8 Km"));
+        nearbyList.add(new Nearby(R.drawable.saigon3, "Luxvoy Luxury Hotel South Sai Gon 3", "5.8 Km"));
+
+        NearbyAdapter nearbyAdapter = new NearbyAdapter(nearbyList, getActivity());
+        rvNearby.setAdapter(nearbyAdapter);
+
+        rvViewed = rootView.findViewById(R.id.rvViewed);
+        rvViewed.setHasFixedSize(true);
+        rvViewed.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<Viewed> viewedList = new ArrayList<>();
+        viewedList.add(new Viewed(R.drawable.img_viewed1, "Phu Quoc Beach, Luxvoy Collection"));
+        viewedList.add(new Viewed(R.drawable.img_viewed1, "Ha Long Bay, Luxvoy Collection"));
+        viewedList.add(new Viewed(R.drawable.img_viewed1, "Da Nang Beach, Luxvoy Collection"));
+        viewedList.add(new Viewed(R.drawable.img_viewed1, "Nha Trang Beach, Luxvoy Collection"));
+
+        ViewedAdapter viewedAdapter = new ViewedAdapter(viewedList, getActivity());
+        rvViewed.setAdapter(viewedAdapter);
+
+        rvDestination = rootView.findViewById(R.id.rvDestinations);
+        rvDestination.setHasFixedSize(true);
+        rvDestination.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<Explore> exploreList = new ArrayList<>();
+        exploreList.add(new Explore(R.drawable.img_explore, "Ha Noi explore"));
+        exploreList.add(new Explore(R.drawable.img_explore, "Da Nang explore"));
+        exploreList.add(new Explore(R.drawable.img_explore, "Nha Trang explore"));
+
+        ExploreAdaper exploreAdaper = new ExploreAdaper(exploreList, getActivity());
+        rvDestination.setAdapter(exploreAdaper);
+
+        rvLuxury = rootView.findViewById(R.id.rvLuxury);
+        rvLuxury.setHasFixedSize(true);
+        rvLuxury.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<Luxury> luxuryList = new ArrayList<>();
+        luxuryList.add(new Luxury(R.drawable.premium1, "Luxury Hotel 1"));
+        luxuryList.add(new Luxury(R.drawable.premium2, "Luxury Hotel 2"));
+        luxuryList.add(new Luxury(R.drawable.premium3, "Luxury Hotel 3"));
+
+        LuxuryAdapter luxuryAdapter = new LuxuryAdapter(luxuryList, getActivity());
+        rvLuxury.setAdapter(luxuryAdapter);
+
+        rvPremium = rootView.findViewById(R.id.rvPremium);
+        rvPremium.setHasFixedSize(true);
+        rvPremium.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        List< Premium > premiumList = new ArrayList<>();
+        premiumList.add(new Premium(R.drawable.premium3, "Premium Hotel 1"));
+        premiumList.add(new Premium(R.drawable.premium4, "Premium Hotel 2"));
+        premiumList.add(new Premium(R.drawable.premium2, "Premium Hotel 3"));
+
+        PremiumAdapter premiumAdapter = new PremiumAdapter(premiumList, getActivity());
+        rvPremium.setAdapter(premiumAdapter);
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/example/hotel_luxvoy/models/Nearby.java
@@ -0,0 +1,42 @@
+package com.example.hotel_luxvoy.models;
+
+public class Nearby {
+
+
+    private int image;
+    private String name;
+    private String address;
+
+    public Nearby() {
+    }
+
+    public Nearby(int image, String name, String address) {
+        this.image = image;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
