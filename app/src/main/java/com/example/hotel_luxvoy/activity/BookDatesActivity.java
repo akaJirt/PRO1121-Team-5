c87487b446e54b6c3670f543ef4f5bb4d63b2783
MinhTri <luongminhtri2411@gmail.com>
2023-08-07 01:36:25 +0700

    Update FE 7/8

--- app/src/main/java/com/example/hotel_luxvoy/activity/BookDatesActivity.java
+++ app/src/main/java/com/example/hotel_luxvoy/activity/BookDatesActivity.java
@@ -3,6 +3,9 @@ package com.example.hotel_luxvoy.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,9 @@ import com.example.hotel_luxvoy.FullScreenHelper;
 import com.example.hotel_luxvoy.R;
 
 public class BookDatesActivity extends AppCompatActivity {
+    ImageView ivBack;
+    TextView tvCheckIn, tvCheckOut;
+    CalendarView cvCheckIn, cvCheckOut;
     Button btnContinue;
 
     @Override
@@ -17,6 +23,28 @@ public class BookDatesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FullScreenHelper.setFullScreen(this);
         setContentView(R.layout.activity_booking_dates);
+        ivBack = findViewById(R.id.ivBack);
+        tvCheckIn = findViewById(R.id.tvCheckInDate);
+        tvCheckOut = findViewById(R.id.tvCheckOutDate);
+        cvCheckIn = findViewById(R.id.calendarCheckIn);
+        cvCheckOut = findViewById(R.id.calendarCheckOut);
+
+        ivBack.setOnClickListener(v -> {
+            Intent intent = new Intent(BookDatesActivity.this, BookLocationActivity.class);
+            startActivity(intent);
+        });
+
+        cvCheckIn.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+            tvCheckIn.setText(date);
+        });
+
+        cvCheckOut.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+            tvCheckOut.setText(date);
+        });
+
+
         btnContinue = findViewById(R.id.btnContinue);
         btnContinue.setOnClickListener(v -> {
             Intent intent = new Intent(BookDatesActivity.this, HotelActivity.class);
